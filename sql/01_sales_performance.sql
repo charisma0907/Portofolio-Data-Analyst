4e01259d78c7c3199329e2bdfd7440d122314278
charisma0907 <wahyucharisma0907@gmail.com>
2026-09-01 18:12:14 +0700

    Update 01_sales_performance.sql

--- sql/01_sales_performance.sql
+++ sql/01_sales_performance.sql
@@ -1,32 +1,55 @@
 /*
-====================================
-Sales Performance Analysis
-====================================
+==========================================================
+ANALISIS PERFORMA PENJUALAN
+==========================================================
+
+Tujuan Analisis:
+Menganalisis performa penjualan perusahaan berdasarkan
+jumlah transaksi, total pendapatan, total produk terjual,
+dan rata-rata nilai transaksi.
+
+Tabel yang digunakan:
+1. orders
+   Digunakan untuk menghitung jumlah transaksi.
+
+2. order_items
+   Digunakan untuk menghitung total pendapatan,
+   total produk terjual, dan rata-rata nilai transaksi.
+
+==========================================================
 */
 
--- Business Question 1
--- Total Transactions
+------------------------------------------------------------
+-- Pertanyaan Bisnis 1
+-- Berapa jumlah transaksi yang terjadi?
+------------------------------------------------------------
 
 SELECT
     COUNT(*) AS total_transactions
 FROM orders;
 
--- Business Question 2
--- Total Revenue
+------------------------------------------------------------
+-- Pertanyaan Bisnis 2
+-- Berapa total pendapatan yang diperoleh perusahaan?
+------------------------------------------------------------
 
 SELECT
     SUM(subtotal_idr) AS total_revenue
 FROM order_items;
 
--- Business Question 3
--- Total Products Sold
+------------------------------------------------------------
+-- Pertanyaan Bisnis 3
+-- Berapa total produk yang berhasil terjual?
+------------------------------------------------------------
 
 SELECT
     SUM(quantity) AS total_products_sold
 FROM order_items;
 
--- Business Question 4
--- Average Order Value
+------------------------------------------------------------
+-- Pertanyaan Bisnis 4
+-- Berapa rata-rata nilai transaksi pelanggan (AOV)?
+------------------------------------------------------------
 
 SELECT
     ROUND(
